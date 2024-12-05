ef954d3d7c8912464ed37d8d86dda4c19e7faf3a
Stephanie Graessle <s.graessle27@gmail.com>
2024-12-05 11:52:54 +0200

    Upload file

---
+++ exchange.py
@@ -0,0 +1,8 @@
+"""
+File: exchange.py
+Author: Stephanie Graessle
+Date: 2024-12-05
+Description: Given every participant in an exchange has a number, randomly
+match another correspondant up with their number without repeating numbers.
+"""
+
